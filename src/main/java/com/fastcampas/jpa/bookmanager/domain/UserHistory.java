43412e826bab1c7a5eab19afb6c9287c0f7eff15
sunwoo22 <mygummy2@gmail.com>
2021-10-08 22:59:07 +0900

    testtest

---
+++ src/main/java/com/fastcampas/jpa/bookmanager/domain/UserHistory.java
@@ -0,0 +1,33 @@
+package com.fastcampas.jpa.bookmanager.domain;
+
+import com.fastcampas.jpa.bookmanager.domain.listener.Auditable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class UserHistory extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    @ManyToOne
+    private User user;
+
+}
---
+++ src/main/java/com/fastcampas/jpa/bookmanager/domain/listener/UserEntityListener.java
@@ -0,0 +1,32 @@
+package com.fastcampas.jpa.bookmanager.domain.listener;
+
+import com.fastcampas.jpa.bookmanager.domain.User;
+import com.fastcampas.jpa.bookmanager.domain.UserHistory;
+import com.fastcampas.jpa.bookmanager.repository.UserHistoryRepository;
+import com.fastcampas.jpa.bookmanager.support.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.PostPersist;
+import javax.persistence.PostUpdate;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+public class UserEntityListener {
+
+    @PostPersist
+    @PostUpdate
+    public void prePersistAndPreUpdate(Object o) {
+        UserHistoryRepository userHistoryRepository = BeanUtils.getBean(UserHistoryRepository.class);
+
+        User user = (User) o;
+
+        UserHistory userHistory = new UserHistory();
+        userHistory.setName(user.getName());
+        userHistory.setEmail(user.getEmail());
+        userHistory.setUser(user);
+
+        userHistoryRepository.save(userHistory);
+    }
+}
